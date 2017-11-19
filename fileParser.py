758b2582054d4eba3c5054db6ffc341b523134bd
jessekunze <Kunze.jesse@gmail.com>
2017-11-19 16:00:05 -0600

    Added file parsing

---
+++ fileParser.py
@@ -0,0 +1,11 @@
+import csv
+
+INPUT_FILE = "data.csv"
+NUM_MIN_GAMES = 5
+
+with open(INPUT_FILE, 'r') as i, open('data_parsed.csv', 'w') as o:
+    writer = csv.writer(o)
+    for row in csv.reader(i):
+        if int(row[8]) >= NUM_MIN_GAMES:
+            writer.writerow(row)
+
