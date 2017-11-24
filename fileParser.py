878edeae09ad80d13f2c947cf7f06401569426c0
jessekunze <Kunze.jesse@gmail.com>
2017-11-24 14:39:31 -0600

    Fixed a bug where new lines were in parsed CSV

--- fileParser.py
+++ fileParser.py
@@ -1,9 +1,11 @@
 import csv
+import fileinput
+
 
 INPUT_FILE = "data.csv"
 NUM_MIN_GAMES = 5
 
-with open(INPUT_FILE, 'r') as i, open('data_parsed.csv', 'w') as o:
+with open(INPUT_FILE, 'r') as i, open('data_parsed.csv', 'w', newline='') as o:
     writer = csv.writer(o)
     for row in csv.reader(i):
         if int(row[8]) >= NUM_MIN_GAMES:
