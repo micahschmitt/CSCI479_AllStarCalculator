7079db2541615dcb21c5afd6e686b76917c66cea
micahschmitt <micah.schmitt@ndsu.edu>
2017-11-26 21:18:21 -0600

    Initial commit

--- kMeans.py
+++ kMeans.py
@@ -1,1 +1,6 @@
+import numpy as np
+import scipy
+from sklearn.cluster import KMeans
+
 print "K Means Algorithm"
+
--- logisticRegression.py
+++ logisticRegression.py
@@ -1,3 +1,8 @@
-import csv
+#Author: Micah Schmitt
+import numpy
+import sklearn
 
 print "Logistic Regression Algorithm"
+
+# We're gonna take in the already parsed file and use the kMeans algo to calc
+# who should be an all star
