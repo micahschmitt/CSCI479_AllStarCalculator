0fab92f0b3e57637bd7daace9b3638ec74e27073
jessekunze <Kunze.jesse@gmail.com>
2017-11-27 18:23:04 -0600

    Added precision and recall

--- LinearDiscriminantAnalysis.py
+++ LinearDiscriminantAnalysis.py
@@ -1,6 +1,7 @@
 import numpy as np
 from sklearn import cross_validation
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.metrics import precision_score, accuracy_score, make_scorer
 
 FOLDS = 10
 
@@ -13,11 +14,16 @@ A = A[:, 1:]
 
 lda = LinearDiscriminantAnalysis()
 lda.fit(A, y)
-drA = lda.transform(A)
+lda.transform(A)
 
 print('Results: ')
 #Performs and prints k-folds validation
-scores = cross_validation.cross_val_score(lda, A, y, cv=10, scoring='accuracy')
-print(scores)
-print("Average: ")
-print(scores.mean())
+accuracy = cross_validation.cross_val_score(lda, A, y, cv=10, scoring='accuracy').mean()
+precision = cross_validation.cross_val_score(lda, A, y, cv=10, scoring='precision').mean()
+recall = cross_validation.cross_val_score(lda, A, y, cv=10, scoring='recall').mean()
+print("Average Accuracy: ")
+print(accuracy)
+print("Precision: ")
+print(precision)
+print("Recall: ")
+print(recall)
