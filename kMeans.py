cb3b241e0e885d10e77f9d8529a29700ae431834
micahschmitt <micah.schmitt@ndsu.edu>
2017-12-03 08:46:37 -0600

    Finished the kmeans and logistic regression algs. Need to look at the crossval precision and other since it does not work with my local.

--- kMeans.py
+++ kMeans.py
@@ -1,14 +1,35 @@
 import numpy as np
-import scipy
-from sklearn.cluster import KMeans
+from sklearn import cross_validation
+from sklearn.cluster import KMeans, MiniBatchKMeans
 
 print "K Means Algorithm"
 
 #Read in data from the file
 stats = np.loadtxt('parsed_cleaned_stats.txt', delimiter=' ')
 
-km = KMeans(n_clusters=3, init=stats, n_init=1)
+y = stats[:, 0]
+stats = stats[:, 1:]
 
+mb = MiniBatchKMeans(n_clusters=2)
+km = KMeans(n_clusters=2)
+mb.fit(stats, y)
+km.fit(stats, y)#, init=stats, n_init=1)
+#km.transform(stats)
+
+accuracy1 = cross_validation.cross_val_score(km, stats, y, cv=10, scoring='accuracy').mean()
+accuracy2 = cross_validation.cross_val_score(mb, stats, y, cv=10, scoring='accuracy').mean()
+#precision = cross_validation.cross_val_score(km, stats, y, cv=10, scoring='precision').mean()
+
+print "Results: "
+print
+#print scores
+print 
+print "Average accuracy kmeans: "
+print accuracy1
+print "Average accuracy minibatchkmeans: "
+print accuracy2
+#print "Average precision: "
+#print precision
 #print km
 #centers = km.centroid
-print km# centers
+#print km# centers
--- logisticRegression.py
+++ logisticRegression.py
@@ -1,7 +1,19 @@
 import numpy as np
 from sklearn import linear_model
+from sklearn import cross_validation
 
 print "Logistic Regression Algorithm"
 
 stats = np.loadtxt('parsed_cleaned_stats.txt', delimiter=' ')
+y = stats[:, 0]
+stats = stats[:, 1:]
 
+logreg = linear_model.LogisticRegression()
+logreg.fit(stats, y)
+
+print "Results: "
+
+accuracy = cross_validation.cross_val_score(logreg, stats, y, cv=10, scoring='accuracy').mean()
+
+print "Average accuracy: "
+print accuracy
