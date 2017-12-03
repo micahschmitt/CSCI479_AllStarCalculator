dfdcc0fffe85f33b8fdec53dd3c876024d15c826
micahschmitt <micah.schmitt@ndsu.edu>
2017-12-03 16:16:29 -0600

    fixed kmeans

--- kMeans.py
+++ kMeans.py
@@ -10,24 +10,23 @@ stats = np.loadtxt('parsed_cleaned_stats.txt', delimiter=' ')
 y = stats[:, 0]
 stats = stats[:, 1:]
 
-mb = MiniBatchKMeans(n_clusters=2)
 km = KMeans(n_clusters=2)
-mb.fit(stats, y)
 km.fit(stats, y)#, init=stats, n_init=1)
-#km.transform(stats)
 
-accuracy1 = cross_validation.cross_val_score(km, stats, y, cv=10, scoring='accuracy').mean()
-accuracy2 = cross_validation.cross_val_score(mb, stats, y, cv=10, scoring='accuracy').mean()
-#precision = cross_validation.cross_val_score(km, stats, y, cv=10, scoring='precision').mean()
+accuracy = cross_validation.cross_val_score(km, stats, y, cv=10, scoring='accuracy').mean()
+precision = cross_validation.cross_val_score(km, stats, y, cv=10, scoring='precision').mean()
+recall = cross_validation.cross_val.score(km, stats, y, cv=10, scoring='recall').mean()
 
 print "Results: "
 print
 #print scores
 print 
-print "Average accuracy kmeans: "
-print accuracy1
-print "Average accuracy minibatchkmeans: "
-print accuracy2
+print "Average accuracy: "
+print accuracy
+print "Average precision: "
+print precision
+print "Average recall: "
+print recall
 #print "Average precision: "
 #print precision
 #print km
